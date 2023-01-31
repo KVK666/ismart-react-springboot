c751731a8f21f54a267422304bdcb3e7a698c419
KVK666 <vamsik823@gmail.com>
2023-02-01 00:10:56 +0530

    added search box

--- src/ReviewApp/MovieListComponent.jsx
+++ src/ReviewApp/MovieListComponent.jsx
@@ -7,6 +7,8 @@ import "./MovieReviewApp.css"
 
 function MovieListComponent() {
     const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState("");
+
 
     const [message, setMessage] = useState([])
 
@@ -66,12 +68,24 @@ function MovieListComponent() {
         navigate(`/movies/${userName}/reviews/${id}`)
     }
 
+    function handleSearch(event) {
+        setSearchTerm(event.target.value);
+    }
+
 
 
     return (
         <div className='Container'>
-            <h1>Movies            {isAdmin && <div style={{ float: "right" }} className="btn btn-success" onClick={addNewMovie}> Add New Movie</div>}
+            <h1 style={{display: "flex", justifyContent: "space-between"}}>Movies{isAdmin && <div className="btn btn-success" onClick={addNewMovie}> Add New Movie</div>}
             </h1>
+            <input
+                style={{width: "30%", marginRight: "10px"}}
+                type="text"
+                placeholder="Search for a movie"
+                value={searchTerm}
+                onChange={handleSearch}
+            />
+
 
             {message && <div className="alert alert-warning">{message}</div>}
             <div>
@@ -92,23 +106,25 @@ function MovieListComponent() {
                         </tr>
                     </thead>
                     <tbody>{
-                        movies.map(
-                            movie => (
-                                <tr key={movie.id}>
-                                    {/* <td>{movie.id}</td> */}
-                                    <td className="title" onClick={() => moviePreview(authContext.userName, movie.id)}>{movie.title}</td>
-                                    <td className="poster" onClick={() => moviePreview(authContext.userName, movie.id)}><img src={movie.poster} alt={movie.title} /></td>
-                                    <td>{movie.year}</td>
-                                    <td>{movie.description}</td>
-                                    <td>{movie.director}</td>
-                                    <td>{movie.language}</td>
-                                    {/* <td>{movie.overAllRating}</td> */}
-                                    {isAdmin && <td><button className="btn btn-success" onClick={() => updateMovie(movie.id)}>Update</button></td>}
-                                    {isAdmin && <td><button className="btn btn-warning" onClick={() => deleteMovie(movie.id)}>Delete</button></td>}
-                                </tr>
+                        movies
+                            .filter(movie => movie.title.toLowerCase().includes(searchTerm.toLowerCase()))
+                            .map(
+                                movie => (
+                                    <tr key={movie.id}>
+                                        {/* <td>{movie.id}</td> */}
+                                        <td className="title" onClick={() => moviePreview(authContext.userName, movie.id)}>{movie.title}</td>
+                                        <td className="poster" onClick={() => moviePreview(authContext.userName, movie.id)}><img src={movie.poster} alt={movie.title} /></td>
+                                        <td>{movie.year}</td>
+                                        <td>{movie.description}</td>
+                                        <td>{movie.director}</td>
+                                        <td>{movie.language}</td>
+                                        {/* <td>{movie.overAllRating}</td> */}
+                                        {isAdmin && <td><button className="btn btn-success" onClick={() => updateMovie(movie.id)}>Update</button></td>}
+                                        {isAdmin && <td><button className="btn btn-warning" onClick={() => deleteMovie(movie.id)}>Delete</button></td>}
+                                    </tr>
 
+                                )
                             )
-                        )
                     }
 
                     </tbody>
